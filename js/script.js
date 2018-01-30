8a6c0068b52e44fce3000dc305b6baba40191cee
Aldair <garcia3@citypolyhs.org>
2018-01-30 23:24:06 +0000

    I made mysong come out

--- js/script.js
+++ js/script.js
@@ -19,28 +19,28 @@
 var mySong = {
 	"title":"24K Magic",
 	"artist":"Bruno Mars",
-	"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-	"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+	"mp3url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
+	"imageurl":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
 }
 
 var myPlayList = [
 	{
 		"title":"24K Magic",
 		"artist":"Bruno Mars",
-		"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-		"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+		"mp3url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
+		"imageurl":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
 	},
 	{
 		"title":"Sir Duke",
 		"artist":"Stevie Wonder",
-		"mp3-url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
-		"image-url":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
+		"mp3url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
+		"imageurl":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
 	},
 	{
 		"title":"Sorry",
 		"artist":"Justin Bieber",
-		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
-		"image-url":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
+		"mp3url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
+		"imageurl":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
 	}
 
 ]
@@ -51,19 +51,28 @@ var myPlayList = [
 $( document ).ready(function() {
   
 
-
-
+displayList();
+clearList();
 });
 
 function displayList(){
-
+$('body').append("<p>title: " + mySong.title + "</p>");
+$('body').append("<p>artist: " + mySong.artist + "</p>");
+$('body').append("<p>address: " + mySong.mp3url + "</p>");
+$('body').append("<img src="+mySong.imageurl+">");
 
   
 }
 
 function clearList(){
   
-  
+for(var i=0;i < myPlayList ; i++){
+
+$('body').append("<p>title: " + myPlayList.title + "</p>");
+$('body').append("<p>artist: " + myPlayList.artist + "</p>");
+$('body').append("<p>address: " + myPlayList.mp3url + "</p>");
+$('body').append("<img src="+ myPlayList.imageurl+">");
+}  
   
 }
 
